9743edc176563d7effc8d480a9abdf6e511af15d
Fernando Daciuk <f.daciuk@gmail.com>
2015-03-07 05:02:04 +0000

    Add tests

---
+++ tests/test.ajax.js
@@ -0,0 +1,24 @@
+;(function ( root, factory ) {
+  'use strict';
+  /* istanbul ignore next */
+  if ( typeof define === 'function' && define.amd ) {
+    define( function() {
+      require([ 'Ajax' ]);
+    });
+  }
+  else if ( typeof exports === 'object' ) {
+    exports = module.exports = factory( require( 'chai' ).should(), require( '../src/ajax' ) );
+  }
+  else {
+    root.Ajax = factory( root.chai.should(), root.Ajax );
+  }
+})(this, function( should, Ajax ) {
+  'use strict';
+
+  describe( 'Test module interface', function() {
+    it( 'Should have `get` method', function() {
+      var ajax = new Ajax();
+      ajax.should.have.property( 'get' );
+    });
+  });
+});
