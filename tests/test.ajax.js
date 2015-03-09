51d1f8117604fed858356b25e6c7938b37f7b222
Fernando Daciuk <f.daciuk@gmail.com>
2015-03-09 11:59:05 +0000

    Refactory in test file

--- tests/test.ajax.js
+++ tests/test.ajax.js
@@ -16,70 +16,60 @@
   'use strict';
 
   describe( 'Test module interface', function() {
+    var ajax = new Ajax();
+
     it( 'Should have `get` method', function() {
-      var ajax = new Ajax();
       ajax.should.have.property( 'get' );
     });
 
     it( 'Should have `post` method', function() {
-      var ajax = new Ajax();
       ajax.should.have.property( 'post' );
     });
 
     it( 'Should have `put` method', function() {
-      var ajax = new Ajax();
       ajax.should.have.property( 'put' );
     });
 
     it( 'Should have `delete` method', function() {
-      var ajax = new Ajax();
       ajax.should.have.property( 'delete' );
     });
 
     it( 'Should `get` method returns `done` method', function() {
-      var ajax = new Ajax();
       var getRequest = ajax.get();
       getRequest.should.have.property( 'done' );
     });
 
     it( 'Should `get` method returns `error` method', function() {
-      var ajax = new Ajax();
       var getRequest = ajax.get();
       getRequest.should.have.property( 'error' );
     });
 
     it( 'Should `post` method returns `done` method', function() {
-      var ajax = new Ajax();
       var postRequest = ajax.post();
       postRequest.should.have.property( 'done' );
     });
 
     it( 'Should `post` method returns `error` method', function() {
-      var ajax = new Ajax();
       var postRequest = ajax.post();
       postRequest.should.have.property( 'error' );
     });
 
     it( 'Should `put` method returns `done` method', function() {
-      var ajax = new Ajax();
       var putRequest = ajax.put();
       putRequest.should.have.property( 'done' );
     });
 
     it( 'Should `put` method returns `error` method', function() {
-      var ajax = new Ajax();
       var putRequest = ajax.put();
       putRequest.should.have.property( 'error' );
     });
 
     it( 'Should `delete` method returns `done` method', function() {
-      var ajax = new Ajax();
       var deleteRequest = ajax.delete();
       deleteRequest.should.have.property( 'done' );
     });
 
     it( 'Should `delete` method returns `error` method', function() {
-      var ajax = new Ajax();
       var deleteRequest = ajax.delete();
       deleteRequest.should.have.property( 'error' );
     });
