37fec79bca046dcbd29c74945621e4cab8a61ce2
Fernando Daciuk <f.daciuk@gmail.com>
2015-04-27 22:45:02 -0300

    Move routes to main api file

--- api/app.js
+++ api/app.js
@@ -3,7 +3,35 @@
 var connect = require( 'connect' );
 var app = connect();
 var PORT = 3000;
-require( './routes' );
+
+var connectRoute = require( 'connect-route' );
+var bodyParser = require( 'body-parser' );
+var users = require( './data/users' );
+
+app.use( bodyParser.urlencoded({ extended: false }) );
+app.use( bodyParser.json() );
+app.use(function( req, res, next ) {
+  res.setHeader( 'Access-Control-Allow-Origin', '*' );
+  next();
+});
+
+app.use( connectRoute( function( router ) {
+  function postRequest( req, res, next ) {
+    var user = req.params.slug || req.body.slug;
+    res.setHeader( 'Content-Type', 'application/json' );
+    res.end( JSON.stringify( users[ user ] ) );
+  }
+
+  router.get( '/api/users', function( req, res, next ) {
+    res.setHeader( 'Content-Type', 'application/json' );
+    res.end( JSON.stringify( users ) );
+  });
+
+  router.post( '/api/user', postRequest );
+  router.post( '/api/user/:slug', postRequest );
+  router.put( '/api/user/:slug', postRequest );
+  router.delete( '/api/user/:slug', postRequest );
+}));
 
 app.listen( PORT );
 console.log( 'Server listen on port ', PORT );
