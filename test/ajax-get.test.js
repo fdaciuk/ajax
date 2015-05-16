4cb402551c136b41364bff1d85e8105924721a45
Fernando Daciuk <f.daciuk@gmail.com>
2015-05-16 17:55:09 -0300

    Improve tests

--- test/ajax-get.test.js
+++ test/ajax-get.test.js
@@ -18,10 +18,25 @@
   'use strict';
 
   describe( '#AJAX - Test `get` method', function() {
+    var ajax = new Ajax();
+
     it( 'Should return an object (users list)', function( done ) {
-      var ajax = new Ajax();
       ajax.get( 'http://127.0.0.1:3000/api/users' ).done(function( response ) {
         response.should.be.an( 'object' );
+        done();
+      });
+    });
+
+    it( 'Should return 404 error', function( done ) {
+      ajax.get( 'http://127.0.0.1:3000/api/something' ).error(function( response, xhr ) {
+        xhr.status.should.be.equal( 404 );
+        done();
+      });
+    });
+
+    it( 'Should return 404 error on `always` promise', function( done ) {
+      ajax.get( 'http://127.0.0.1:3000/api/something' ).always(function( response, xhr ) {
+        xhr.status.should.be.equal( 404 );
         done();
       });
     });
