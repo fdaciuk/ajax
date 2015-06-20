7eb760669a6f303becba5e1b7f32895dfc18b376
Fernando Daciuk <f.daciuk@gmail.com>
2015-06-20 18:43:13 -0300

    Improve methods

--- src/ajax.js
+++ src/ajax.js
@@ -44,7 +44,7 @@
       xhr.open( type, url || '', true );
       xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
       xhr.addEventListener( 'readystatechange', $private.handleReadyStateChange, false );
-      xhr.send( $private.convertObjectToQueryString( data ) );
+      xhr.send( $private.objectToQueryString( data ) );
       return $private.promises();
     };
 
@@ -62,12 +62,8 @@
 
     $private.parseResponse = function parseResponse( xhr ) {
       var result;
-      try {
-        result = JSON.parse( xhr.responseText );
-      }
-      catch( e ) {
-        result = xhr.responseText;
-      }
+      try { result = JSON.parse( xhr.responseText ); }
+      catch( e ) { result = xhr.responseText; }
       return [ result, xhr ];
     };
 
@@ -85,14 +81,13 @@
       };
     };
 
-    $private.convertObjectToQueryString = function convertObjectToQueryString( data ) {
-      var convertedData = [];
-      if( ! $private.isObject( data ) )
+    $private.objectToQueryString = function objectToQueryString( data ) {
+      if( !$private.isObject( data ) )
         return data;
-      Object.keys( data ).forEach(function( key ) {
-        convertedData.push( key + '=' + data[ key ] );
-      });
-      return convertedData.join( '&' );
+
+      return Object.keys( data ).map( function( item ) {
+        return item + '=' + data[ item ];
+      }).join( '&' );
     };
 
     $private.isObject = function isObject( data ) {
