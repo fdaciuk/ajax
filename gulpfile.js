a95da2a18aa5d3b3340aaf65969ac3dcdc23bea4
Fernando Daciuk <f.daciuk@gmail.com>
2015-12-08 23:01:10 -0200

    gulpfile ES6 style

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var gulp = require( 'gulp' );
-var karma = require( 'karma' ).server;
-var jshint = require( 'gulp-jshint' );
-var concat = require( 'gulp-concat' );
-var uglify = require( 'gulp-uglify' );
-var header = require( 'gulp-header' );
-var jscs = require( 'gulp-jscs' );
-var plato = require( 'plato' );
-var exec = require( 'child_process' ).exec;
-var pkg = require( './package.json' );
+let gulp = require( 'gulp' );
+let karma = require( 'karma' ).server;
+let jshint = require( 'gulp-jshint' );
+let concat = require( 'gulp-concat' );
+let uglify = require( 'gulp-uglify' );
+let header = require( 'gulp-header' );
+let jscs = require( 'gulp-jscs' );
+let plato = require( 'plato' );
+let exec = require( 'child_process' ).exec;
+let pkg = require( './package.json' );
 
-var coreFiles = 'src/**/*.js';
-var allFiles = '{test,src}/**/*.js';
+let coreFiles = 'src/**/*.js';
+let allFiles = '{test,src}/**/*.js';
 
 function banner() {
   return [
@@ -28,21 +28,21 @@ function banner() {
   ].join( '\n' );
 }
 
-gulp.task( 'lint', function() {
+gulp.task( 'lint', () => {
   gulp.src( allFiles )
     .pipe( jshint() )
     .pipe( jshint.reporter( 'default' ) );
 
   gulp.src( allFiles )
     .pipe( jscs() )
-    .on( 'error', function( err ) {
-      var ERROR_CODE = 1;
+    .on( 'error', err => {
+      let ERROR_CODE = 1;
       console.log( err.toString() );
       process.exit( ERROR_CODE );
     });
 });
 
-gulp.task( 'uglify', function() {
+gulp.task( 'uglify', () => {
   gulp.src( coreFiles )
     .pipe( concat( 'ajax.min.js' ) )
     .pipe( uglify() )
@@ -50,37 +50,37 @@ gulp.task( 'uglify', function() {
     .pipe( gulp.dest( './dist' ) );
 });
 
-gulp.task( 'test', function( done ) {
+gulp.task( 'test', done => {
   karma.start({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
   }, done );
 });
 
-gulp.task( 'webserver', function( done ) {
+gulp.task( 'webserver', done => {
   require( './api/app' );
-  exec( 'pkill -9 python && python -m SimpleHTTPServer 9001', function() {
+  exec( 'pkill -9 python && python -m SimpleHTTPServer 9001', () => {
     console.log( 'Server listen on port 9001' );
     done();
   });
 });
 
-gulp.task( 'watch', [ 'test', 'lint' ], function() {
+gulp.task( 'watch', [ 'test', 'lint' ], () => {
   gulp.watch( '{test,src}/**/*.js', [ 'test', 'lint' ]);
 });
 
-gulp.task( 'plato', function( done ) {
-  var files = [ coreFiles ];
-  var outputDir = './plato';
-  var options = { title: '#Ajax' };
+gulp.task( 'plato', done => {
+  let files = [ coreFiles ];
+  let outputDir = './plato';
+  let options = { title: '#Ajax' };
   function callback( report ) { done(); };
   plato.inspect( files, outputDir, options, callback );
 });
 
-gulp.task( 'deploy', function( done ) {
+gulp.task( 'deploy', done => {
   console.log( 'Deploying...' );
-  var date = new Date( Date.now() );
-  var commands = [
+  let date = new Date( Date.now() );
+  let commands = [
     'gulp uglify',
     'git add .',
     'git commit -m "Minifying"',
@@ -100,7 +100,7 @@ gulp.task( 'deploy', function( done ) {
     'git push origin master --tags',
     'git checkout dev'
   ];
-  exec( commands.join( ' && ' ), function( err, stdout, stderr ) {
+  exec( commands.join( ' && ' ), ( err, stdout, stderr ) => {
     console.log( 'Done!' );
     if( stdout ) console.log( 'STDOUT:', stdout );
     if( stderr ) console.log( 'STDERR:', stderr );
@@ -110,4 +110,4 @@ gulp.task( 'deploy', function( done ) {
   });
 });
 
-gulp.task( 'default', [ 'webserver', 'watch' ]);
+gulp.task( 'default', [ 'watch' ]);
