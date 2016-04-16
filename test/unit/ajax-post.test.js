8d69a0e7ad9f8071996c0ced527d6cd1a566522b
Fernando Daciuk <f.daciuk@gmail.com>
2016-04-16 11:40:16 -0300

    Add test to send null headers

--- test/unit/ajax-post.test.js
+++ test/unit/ajax-post.test.js
@@ -1,11 +1,11 @@
-;(function (should, expect, Ajax) {
+;(function (should, expect, Ajax, ajax) {
   'use strict'
 
   describe('#AJAX - Test `post` method', function () {
-    var ajax = new Ajax()
+    var request = new Ajax()
 
     it('DEPRECATED - Should return an object', function (done) {
-      ajax.post('http://127.0.0.1:3000/api/user/joao')
+      request.post('http://127.0.0.1:3000/api/user/joao')
         .done(function (response) {
           response.should.be.an('object')
           done()
@@ -13,7 +13,7 @@
     })
 
     it('Should return an object', function (done) {
-      ajax.post('http://127.0.0.1:3000/api/user/joao')
+      request.post('http://127.0.0.1:3000/api/user/joao')
         .then(function (response) {
           response.should.be.an('object')
           done()
@@ -21,7 +21,7 @@
     })
 
     it('DEPRECATED - Should return data about `joao`', function (done) {
-      ajax.post('http://127.0.0.1:3000/api/user', { slug: 'joao' })
+      request.post('http://127.0.0.1:3000/api/user', { slug: 'joao' })
         .done(function (response) {
           response.name.should.be.equal('João da Silva')
           done()
@@ -29,7 +29,7 @@
     })
 
     it('Should return data about `joao`', function (done) {
-      ajax.post('http://127.0.0.1:3000/api/user', { slug: 'joao' })
+      request.post('http://127.0.0.1:3000/api/user', { slug: 'joao' })
         .then(function (response) {
           response.name.should.be.equal('João da Silva')
           done()
@@ -37,7 +37,7 @@
     })
 
     it("DEPRECATED - Should return error 404 when user doesn't exist", function (done) {
-      ajax.post('http://127.0.0.1:3000/api/user', { slug: 'alberto' })
+      request.post('http://127.0.0.1:3000/api/user', { slug: 'alberto' })
         .error(function (response, xhr) {
           xhr.status.should.be.equal(404)
           done()
@@ -45,11 +45,24 @@
     })
 
     it("Should return error 404 when user doesn't exist", function (done) {
-      ajax.post('http://127.0.0.1:3000/api/user', { slug: 'alberto' })
+      request.post('http://127.0.0.1:3000/api/user', { slug: 'alberto' })
         .catch(function (response, xhr) {
           xhr.status.should.be.equal(404)
           done()
         })
     })
+
+    it('Should not send some header if it is null', function (done) {
+      var file = new window.FormData()
+      file.append('file', 'test')
+      var request = ajax({
+        headers: { 'content-type': null }
+      })
+      request.post('http://localhost:3000/api/getheader', file)
+        .then(function (response, xhr) {
+          response.header.should.match(/^multipart\/form-data/)
+          done()
+        })
+    })
   })
-})(window.chai.should(), window.chai.expect, window.Ajax)
+})(window.chai.should(), window.chai.expect, window.Ajax, window.ajax)
