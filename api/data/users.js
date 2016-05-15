975271185be9d27d61a71756998730b8f3e0353a
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-14 23:05:05 -0300

    Add new user for tests

--- api/data/users.js
+++ api/data/users.js
@@ -3,5 +3,6 @@
 exports = module.exports = {
   joao: { name: 'João da Silva', age: 30 },
   maria: { name: 'Maria Firmina', age: 26 },
-  paulo: { name: 'Paulo Torres', age: 25 }
+  paulo: { name: 'Paulo Torres', age: 25 },
+  lazy: { name: 'Lazy user', age: 0 }
 }
