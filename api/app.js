636a866888e244422e663de27346d5fd9e640d93
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-15 12:20:32 -0300

    Update api script

--- api/app.js
+++ api/app.js
@@ -1,22 +1,21 @@
 'use strict'
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
-var multer = require('multer')
-var upload = multer({ dest: 'uploads/' })
-var app = express()
-var PORT = 3000
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const app = express()
 
-var users = require('./data/users')
+const users = require('./data/users')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
 function handleRequest (req, res, next) {
-  var userRequested = req.params.slug || req.body.slug
-  var user = userRequested ? users[ userRequested ] : users
+  const userRequested = req.params.slug || req.body.slug
+  let user = userRequested ? users[ userRequested ] : users
   if (!user) {
     res.status(404)
     user = '404 - Not found'
@@ -41,10 +40,7 @@ app.post('/api/formdata', upload.any(), (req, res) => {
   res.json({ body: req.body, files: req.files })
 })
 app.post('/api/getheader', (req, res) => {
-  res.json({ header: req.get('content-type' )})
+  res.json({ header: req.get('content-type') })
 })
-
-app.listen(PORT)
-console.log('Server listen on port ', PORT)
 
 exports = module.exports = app
