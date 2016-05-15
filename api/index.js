a344f9e106f2358d9225c8aca44d97f351d1771f
Fernando Daciuk <f.daciuk@gmail.com>
2016-05-15 12:45:01 -0300

    Add process to exit server

--- api/index.js
+++ api/index.js
@@ -2,7 +2,20 @@
 
 const app = require('./app')
 const PORT = 3000
+const server = app.listen(PORT)
 
-app.listen(PORT)
+const gracefulShutdown = () => {
+  console.log('Server is closing...')
+  server.close(() => {
+    console.log('Server closed.')
+    process.exit()
+  })
+
+  setTimeout(() => process.exit(), 3000)
+}
+
+process.on('SIGINT', () => gracefulShutdown())
+process.on('SIGTERM', () => gracefulShutdown())
+
 console.log('Server listen on port ', PORT)
 
